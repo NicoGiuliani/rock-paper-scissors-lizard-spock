4c298e418559ec927c81d2b7b4ef16bf04722c9e
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-09-28 21:56:47 -0700

    Got game logic working

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -14,12 +14,74 @@ export class GameComponent {
     { name: "lizard", imageUrl: "", winsAgainst: ["paper", "spock"], losesTo: ["scissors", "rock"] },
     { name: "spock", imageUrl: "", winsAgainst: ["scissors", "rock"], losesTo: ["lizard", "paper"] }
   ];
+
+  verbChart: { [key: string]: { [key: string]: string }} = {
+     "rock": {
+      "scissors": "smashes",
+      "lizard": "crushes",
+     },
+     "paper": {
+      "rock": "covers",
+      "spock": "disproves",
+     },
+     "scissors": {
+      "paper": "cut",
+      "lizard": "decapitate",
+     },
+     "lizard": {
+      "paper": "eats",
+      "spock": "poisons",
+     },
+     "spock": {
+      "rock": "vaporizes",
+      "scissors": "smashes",
+     },
+  }
   
   playerChoice!: GameChoice;
   opponentChoice!: GameChoice;
+  winner: string | null = null;
+  loser: string | null = null;
+  gameResult!: string;
+  verb!: string;
+  outputExpression!: string;
 
   onSetSelection(selection: GameChoice) {
     this.playerChoice = selection;
-    console.log("player chose:", this.playerChoice);
+    this.getOpponentChoice();
+    this.getResult();
+    this.getOutputExpression();
+
+    console.log("You chose:", this.playerChoice.name);
+    console.log("Opponent chose:", this.opponentChoice.name);
+    console.log(this.outputExpression);
   }
+
+  getOpponentChoice() {
+    this.opponentChoice = this.gameOptions[Math.floor(Math.random() * 5)];
+  }
+
+  getResult() {
+    if (this.playerChoice.winsAgainst.includes(this.opponentChoice.name)) {
+      this.winner = this.playerChoice.name;
+      this.loser = this.opponentChoice.name;
+      this.gameResult = "WIN";
+    } else if (this.playerChoice.losesTo.includes(this.opponentChoice.name)) {
+      this.winner = this.opponentChoice.name;
+      this.loser = this.playerChoice.name;
+      this.gameResult = "LOSE";
+    } else {
+      this.gameResult = "DRAW";
+    }
+  }
+
+  getOutputExpression() {
+    this.outputExpression = this.gameResult === "WIN" ? "You win! " : 
+                            this.gameResult === "LOSE" ? "You lose! " : "Draw! ";
+    if (this.winner !== null && this.loser !== null) {
+      this.verb = this.verbChart[this.winner][this.loser];
+      this.outputExpression += this.winner + " " + this.verb + " " + this.loser + "!";
+    }
+  }
+
 }
